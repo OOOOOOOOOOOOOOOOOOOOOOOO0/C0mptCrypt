4eb7481b68eaf99465481e6f4dc68e38953b3d0c
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-01 22:43:57 +0000

    Create decrypt_example.py

---
+++ Examples/decrypt_example.py
@@ -0,0 +1,7 @@
+from C0mptCrypt import C0mptCrypt
+
+string = "4Hx/fDeZ1e9l6PEwDivK1EjGepJzwimQKsBa5rHseAAPA26/Q/32TtGlBLemMQ9p"
+
+decrypted_string = C0mptCrypt().Decrypt(string)
+
+print(decrypted_string)
