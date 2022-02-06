2c57eef4b92c34876ff5ae8c9c5be12c0a144bc1
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-06 10:23:58 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ with open("requirements.txt") as fp:
     requirements = fp.read().splitlines()
 with open("README.md") as fp:
     readme = fp.read()
+with open("LICENSE") as fp:
+    license = fp.read()
 
 setuptools.setup(
     name="C0mptCrypt",
     description="An object-oriented, minamalistic, simple encryption library in Python.",
     url="https://github.com/execution/C0mptCrypt",
     packages=["C0mptCrypt"],
-    license="",
+    license=license,
     install_requires=requirements,
     version=0.1,
     long_description=readme,
