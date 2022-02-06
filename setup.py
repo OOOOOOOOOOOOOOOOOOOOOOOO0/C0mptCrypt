ab097db3944af31d61d601ac7a09353be99e51e3
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-06 10:13:39 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setuptools.setup(
+    name="C0mptCrypt",
+    description=An object-oriented, minamalistic, simple encryption library in Python. ",
+    url="https://github.com/execution/C0mptCrypt",
+    packages=["C0mptCrypt"],
+    license="",
+    install_requires=requirements,
+    version=version,
+    long_description=readme,
+)
