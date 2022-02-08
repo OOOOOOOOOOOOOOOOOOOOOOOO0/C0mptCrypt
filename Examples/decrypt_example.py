683756f151bbf6094427878e6ed9fc312ee9ff26
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-08 06:43:16 +0000

    Delete Examples directory

--- Examples/decrypt_example.py
+++
@@ -1,7 +0,0 @@
-from C0mptCrypt import C0mptCrypt
-
-string = "4Hx/fDeZ1e9l6PEwDivK1EjGepJzwimQKsBa5rHseAAPA26/Q/32TtGlBLemMQ9p"
-
-decrypted_string = C0mptCrypt().Decrypt(string)
-
-print(decrypted_string)
--- Examples/encrypt_example.py
+++
@@ -1,7 +0,0 @@
-from C0mptCrypt import C0mptCrypt
-
-string = "Hi my name is c0mpt0!"
-
-encrypted_string = C0mptCrypt().Encrypt(string)
-
-print(encrypted_string)
